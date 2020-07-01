4b83c0bf2d48c8f15282cdf5b114b1201019cfe4
zhenglize <zhenglizeA@126.com>
2020-07-01 16:53:45 +0800

    二叉树节点

---
+++ tree/Node.java
@@ -0,0 +1,13 @@
+package tree;
+/**
+*@author zhenglize
+  定义二叉树节点
+*/
+public class Node{
+    public int value;
+    public Node left;
+    public Node right;
+    public Node(int data){
+        this.value=data;
+    }
+}
