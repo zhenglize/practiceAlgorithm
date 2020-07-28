9354406e01332f06cd3ddabfb8c92f0512bfcb31
zhenglize <zhenglizeA@126.com>
2020-07-28 13:36:52 +0800

    中缀表达式转后缀表达式实现逆波兰计算器

--- stack/PolandNotation.java
+++ stack/PolandNotation.java
@@ -8,13 +8,107 @@ import java.util.Stack;
 *@author zhenglize
  逆波兰计算器
 */
+class Opera{
+    private static int ADD=1;
+    private static int SUB=1;
+    private static int MUL=2;
+    private static int DIV=2;
+    public static int getValue(String val){
+        int res=0;
+        switch (val){
+            case "+":
+                res=ADD;
+                break;
+            case "-":
+                res=SUB;
+                break;
+            case "*":
+                res=MUL;
+                break;
+            case "/":
+                res=DIV;
+                break;
+            default:
+                System.out.println("您输入的符号有误");
+                break;
+        }
+        return res;
+    }
+}
 public class PolandNotation {
     public static void main(String[] args) {
-        //计算后缀表达式的结果
+        String res="1+((2+3)*4)-5";
+        List<String> list = suffixString(res);
+        System.out.println("中缀表达式为:"+list);
+        List<String> suffixExpression = parseSuffixStringList(list);
+        System.out.println("转换的后缀表达式为:"+suffixExpression);
+
+     /*   //计算后缀表达式的结果
         String suffixExpression="3 4 + 5 * 6 -";
         List<String> listString = getListString(suffixExpression);
         int res = calculate(listString);
-        System.out.println("计算的结果是:"+res);
+        System.out.println("计算的结果是:"+res);*/
+    }
+    //将中缀表达式转换为后缀表达式
+    public static List<String> parseSuffixStringList(List<String> list){
+        //符号栈s1
+        Stack<String> s1=new Stack<>();
+        //由于没有出栈操作，所以使用集合来替代栈
+        List<String> s2=new ArrayList<>();
+        for(String item : list){
+            //如果匹配到的是数字直接入s2
+            if (item.matches("\\d+")){
+                s2.add(item);
+            }
+            else if (item.equals("(")){
+                //如果是左括号直接入s1
+                s1.push(item);
+            }else if (item.equals(")")){
+                //弹出s1中的数字，直到和小括号（匹配尾值
+                while (!s1.peek().equals("(")){
+                    s2.add(s1.pop());
+                }
+                //将(消除
+                s1.pop();
+            }else {
+                //如果是符号，则比较栈顶的优先级
+                while (s1.size()!=0
+                        && Opera.getValue(s1.peek()) >= Opera.getValue(item)){
+                    s2.add(s1.pop());
+                }
+                s1.push(item);
+            }
+        }
+        //若s1中还有符号，则直接弹出到s2中
+        while (!s1.isEmpty()){
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+    //将字符串转换放入list集合中
+    public static List<String> suffixString(String str){
+        List<String> list=new ArrayList<>();
+        int index=0; //记录字符串移动的指针
+        String res="";
+        char c;
+        do {
+            //判断是否为操作符
+            if ((c=str.charAt(index)) <48 || (c=str.charAt(index)) >57){
+                list.add(c+"");
+                index++;
+            }else {
+                res="";
+                //判断数字的位数
+                while ( (index < str.length())  &&
+                        (c=str.charAt(index)) >=48 &&
+                        (c=str.charAt(index)) <=57){
+                    res+=c;
+                    index++;
+                }
+                list.add(res);
+            }
+        }while (index < str.length());
+        return list;
     }
     //将字符串放到集合中
     public static List<String> getListString(String suffix){
